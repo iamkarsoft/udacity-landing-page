ce2e86289050f5a7640214bfc28ec1d4bf153f29
Kofi Ramos Amoussou <karsoft92@gmail.com>
2021-06-23 23:51:24 +0000

    switching to grid

--- js/app.js
+++ js/app.js
@@ -2,6 +2,10 @@
 
 // add class to menu
 let navLinks = document.querySelectorAll('.nav__links--items a');
+const home = document.querySelector('a[href="#content"]');
+const about = document.querySelector('a[href="#about"]');
+const services = document.querySelector('a[href="#services"]');
+const contact = document.querySelector('a[href="#contact"]');
 
 
 
@@ -19,7 +23,42 @@ const previousActive = document.querySelector(".active");
 		previousActive.classList.remove("active");
 	}
 
-	
 	this.classList.add("active");
 }
+
+
+
+window.addEventListener('scroll',function(){
+	const previousActive = document.querySelector(".active");
+
+
+if(window.scrollY < 500  ){
+	if(previousActive){
+		previousActive.classList.remove("active");
+	}
+	home.classList.add("active");
+
+}else if(window.scrollY < 1000){
+	if(previousActive){
+		previousActive.classList.remove("active");
+	}
+	about.classList.add("active");
+}else if(window.scrollY < 1400){
+
+	if(previousActive){
+		previousActive.classList.remove("active");
+	}
+
+	services.classList.add("active");
+}else if(window.scrollY < 2000){
+
+	if(previousActive){
+		previousActive.classList.remove("active");
+	}
+
+	contact.classList.add("active");
+}
+
+
+});
 
